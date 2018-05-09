2a5abd5c176b8d2c20433df994aebab2a0cf71fa
unknown <917549681@qq.com>
2018-05-09 23:04:36 +0800

    add multiprocesse and optimization

--- test_tts.py
+++ test_tts.py
@@ -10,24 +10,37 @@ def tts(name,text,voice):
     exist=main+" "+name+" "+text+" "+voice
     os.system(exist)
 
+def filepath(txtpath,wavpath):
+    for txt in os.listdir(txtpath):
+        name=txt.split('.')
+        p=Pool(2)
+        for i in range(2):
+            if i == 0:
+                p.apply_async(tts,args=(wavpath+'/'+name[0]+'_0.wav',txtpath+'/'+txt,'xiaoyun',))
+            else:
+                p.apply_async(tts,args=(wavpath+'/'+name[0]+'_1.wav',txtpath+'/'+txt,'xiaogang',))
+        p.colse()
+        p.join()
 
-if __name__=='__main__'
+
+if __name__=='__main__':
     os.makedirs('wavdir')
-    for part in os.listdir('./txt'):
-        os.makedirs('wavdir'+'/'+part)
-        wavpart='wavdir'+'/'+part
-        filepart='./txt'+'/'+part
-        for txt in os.listdir(filepart):
-            p=Pool(2)
-            name=txt.split('.')
-            for i in range(2):
-                if i == 0:
-                    p.apply_async(tts,args=(wavpart+'/'+name[0]+'_0.wav',filepart+'/'+txt,'xiaoyun',))
-                else:
-                    p.apply_async(tts,args=(wavpart+'/'+name[0]+'_1.wav',filepart+'/'+txt,'xiaogang',))
-        print('endtime:%s\n' % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+    txtname=os.listdir('./txt')
+    while txtname:
+        file1=txtname.pop()
+        file2=txtname.pop()
+        os.makedirs('wavdir'+'/'+file1)
+        os.makedirs('wavdir'+'/'+file2)
+        wavpart1='wavdir'+'/'+file1
+        wavpart2='wavdir'+'/'+file2
+        filepart1='./txt'+'/'+file1
+        filepart2='./txt'+'/'+file2
+        pid=os.fork()
+        if pid == 0:
+            filepath(filepart1,wavpart1)
+        else:
+            filepath(filepart2,wavpart2)
+    print('endtime:%s\n' % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
             #tts(wavpart+'/'+name[0]+'_0.wav',filepart+'/'+txt,'xiaoyun')
             #tts(wavpart+'/'+name[0]+'_1.wav',filepart+'/'+txt,'xiaogang')
-
-
 
