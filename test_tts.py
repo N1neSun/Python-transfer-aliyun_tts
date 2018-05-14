23df9918ef39e97d675cd2dbbe44c1584c69fe9e
unknown <917549681@qq.com>
2018-05-14 22:59:00 +0800

    optimization

--- test_tts.py
+++ test_tts.py
@@ -2,7 +2,8 @@
 
 import commands
 import os
-import time
+import datetime
+import shutil
 from multiprocessing import Pool
 
 def tts(name,text,voice):
@@ -19,28 +20,36 @@ def filepath(txtpath,wavpath):
                 p.apply_async(tts,args=(wavpath+'/'+name[0]+'_0.wav',txtpath+'/'+txt,'xiaoyun',))
             else:
                 p.apply_async(tts,args=(wavpath+'/'+name[0]+'_1.wav',txtpath+'/'+txt,'xiaogang',))
-        p.colse()
+        p.close()
         p.join()
+
+def init(txt_name):
+    file_1=txt_name.pop()
+    file_2=txt_name.pop()
+    os.makedirs('wavdir'+'/'+file_1)
+    os.makedirs('wavdir'+'/'+file_2)
+    filepart_1='./txt'+'/'+file_1
+    filepart_2='./txt'+'/'+file_2
+    wavpart_1=filepart_1.replace('./txt','wavdir')
+    wavpart_2=filepart_2.replace('./txt','wavdir')
+    return filepart_1,filepart_2,wavpart_1,wavpart_2
 
 
 if __name__=='__main__':
+    start=datetime.datetime.now()
+    if os.path.exists('wavdir'):
+        os.system('rm -rf wavdir')
     os.makedirs('wavdir')
     txtname=os.listdir('./txt')
     while txtname:
-        file1=txtname.pop()
-        file2=txtname.pop()
-        os.makedirs('wavdir'+'/'+file1)
-        os.makedirs('wavdir'+'/'+file2)
-        wavpart1='wavdir'+'/'+file1
-        wavpart2='wavdir'+'/'+file2
-        filepart1='./txt'+'/'+file1
-        filepart2='./txt'+'/'+file2
+        filepart_1,filepart_2,wavpart_1,wavpart_2=init(txtname)
         pid=os.fork()
         if pid == 0:
-            filepath(filepart1,wavpart1)
+            filepath(filepart_1,wavpart_1)
         else:
-            filepath(filepart2,wavpart2)
-    print('endtime:%s\n' % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+            filepath(filepart_2,wavpart_2)
+    end=datetime.datetime.now()
+    print(end-start)
             #tts(wavpart+'/'+name[0]+'_0.wav',filepart+'/'+txt,'xiaoyun')
             #tts(wavpart+'/'+name[0]+'_1.wav',filepart+'/'+txt,'xiaogang')
 
